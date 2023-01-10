a4626f00bebebbdf7507d6bbc873cde829b4b214
Shashikant Shrivastava <89238030+shashikant13@users.noreply.github.com>
2023-01-10 23:17:28 +0530

    Add files via upload

---
+++ Bitwise operator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /*int x=11,y=5,z;
+    z=x&y; And opration
+    z=x|y; or opration
+    z=x^y; x-or operation
+    cout<<z<<endl;*/
+
+    char x=5,y;
+    y=~x;
+    cout<<(int)y<<endl;
+
+    return 0;
+}

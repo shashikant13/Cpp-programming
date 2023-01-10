4d790c80c87a09900e874fe022db6450ac4c6c9d
Shashikant Shrivastava <89238030+shashikant13@users.noreply.github.com>
2023-01-10 23:08:53 +0530

    Add files via upload

---
+++ Change case from upper to lower.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string str="WELCOME";
+
+    for(int i=0;str[i]!='\0';i++)
+    {
+        str[i]=str[i]+32;
+
+    }
+    cout<<str<<endl;
+    return 0;
+}

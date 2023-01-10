d1d78e66d6728fb403da202ad6067617d06e75ce
Shashikant Shrivastava <89238030+shashikant13@users.noreply.github.com>
2023-01-10 23:16:57 +0530

    Add files via upload

---
+++ Area of Tri.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    float b, h, a;
+    cout<<"Enter base & height";
+    cin>>b>>h;
+    a = (b*h)/2;
+    cout<<"Area is "<<a;
+    return 0;
+}
